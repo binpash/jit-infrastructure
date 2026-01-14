25094ec0dd2617128b0f4233c1e6090b11cfc2f6
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2026-01-14 15:51:11 +0100

    nit

--- setup.sh
+++ setup.sh
@@ -6,15 +6,10 @@ cd "$(dirname "$0")"
 PASH_TOP=${PASH_TOP:-$(git rev-parse --show-toplevel)}
 
 cd $PASH_TOP
-# . "$PASH_TOP/scripts/utils.sh"
-# read_cmd_args $@
 
 LOG_DIR=$PASH_TOP/install_logs
 mkdir -p $LOG_DIR
 PYTHON_PKG_DIR=$PASH_TOP/python_pkgs
-
-# Remove old installation if it exists
-rm -rf $PYTHON_PKG_DIR
 
 # Check if venv module is available
 if ! python3 -m venv --help &> /dev/null; then
@@ -33,19 +28,14 @@ echo "Installing Python dependencies..."
 $PYTHON_PKG_DIR/bin/pip install --upgrade pip
 $PYTHON_PKG_DIR/bin/pip install -r "$PASH_TOP/requirements.txt"
 
-# Optional: Install evaluation dependencies if needed
-if [[ "$install_eval" == 1 ]]; then
-    $PYTHON_PKG_DIR/bin/pip install numpy matplotlib
-fi
-
 echo "Building runtime tools..."
 cd "$PASH_TOP/runtime/"
 case "$distro" in
     freebsd*) 
-        gmake #&> $LOG_DIR/make.log
+        gmake
         ;;
     *)
-        make #&> $LOG_DIR/make.log
+        make
         ;;
 esac
 
